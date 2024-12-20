09aa809bbd48a825067f1603bd9a1baa585039bb
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-20 10:50:00 -0300

    feat: I added the packages to do all the tests on the components, I tested the page elements, buttons, I created a mock of the tags

---
+++ test/JobsContext.test.jsx
@@ -0,0 +1,58 @@
+import { screen, render, fireEvent } from "@testing-library/react";
+import { JobsContent } from "../components/JobsContent";
+import { TagProvider } from "../components/Context/TagContext";
+import { useTag } from "../components/Context/TagContext";
+describe("JobsContent", () => {
+    it('should render the JobsContent', () => {
+        render(
+            <TagProvider>
+                <JobsContent />
+            </TagProvider>
+        )
+        const Context = screen.getByText('Photosnap')
+        expect(Context).toBeInTheDocument();
+    })
+
+    it('should filter jobs based on tags', () => {
+        render(
+            <TagProvider>
+                <JobsContent />
+            </TagProvider>
+        )
+        const context = screen.getByText('Manage')
+        expect(context).toBeInTheDocument();
+        const AddTagComponent = () => {
+            const { addTag } = useTag()
+            addTag('Frontend')
+        }
+        render(
+            <TagProvider>
+                <JobsContent />
+                <AddTagComponent />
+            </TagProvider>
+        );
+    })
+
+    it('should filter click jobs Based on Tags', () => {
+
+        const MockClick = jest.fn()
+        const tags = ['Frontend', 'Senior', 'HTML', 'CSS', 'JavaScript']
+
+        render(
+            <TagProvider>
+                <JobsContent />
+            </TagProvider>
+        )
+
+        tags.forEach((tag) => {
+            const tagButtons = screen.getAllByText(tag);
+            expect(tagButtons.length).toBeGreaterThan(0);
+
+            fireEvent.click(tagButtons[0]);
+
+            MockClick(tag);
+        });
+
+    })
+
+})
